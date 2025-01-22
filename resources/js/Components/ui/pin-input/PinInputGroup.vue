a60122dc1138ead3d0a898bfa22bcdf22904b555
Adrián Nuhacet <adnumaro@gmail.com>
2025-01-23 00:49:34 +0100

    add shadcn vue dependency and some components

---
+++ resources/js/Components/ui/pin-input/PinInputGroup.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { Primitive, useForwardProps } from 'radix-vue';
+import { computed } from 'vue';
+
+const props = defineProps({
+  asChild: { type: Boolean, required: false },
+  as: { type: null, required: false },
+  class: { type: null, required: false },
+});
+const delegatedProps = computed(() => {
+  const { class: _, ...delegated } = props;
+  return delegated;
+});
+const forwardedProps = useForwardProps(delegatedProps);
+</script>
+
+<template>
+  <Primitive
+    v-bind="forwardedProps"
+    class="flex items-center"
+  >
+    <slot />
+  </Primitive>
+</template>
---
+++ resources/js/Components/ui/pin-input/PinInputInput.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { PinInputInput, useForwardProps } from 'radix-vue';
+import { computed } from 'vue';
+
+const props = defineProps({
+  index: { type: Number, required: true },
+  disabled: { type: Boolean, required: false },
+  asChild: { type: Boolean, required: false },
+  as: { type: null, required: false },
+  class: { type: null, required: false },
+});
+
+const delegatedProps = computed(() => {
+  const { class: _, ...delegated } = props;
+  return delegated;
+});
+
+const forwardedProps = useForwardProps(delegatedProps);
+</script>
+
+<template>
+  <PinInputInput
+    v-bind="forwardedProps"
+    class="relative text-center focus:outline-none focus:ring-2 focus:ring-ring focus:relative focus:z-10 flex h-10 w-10 items-center justify-center border-y border-r border-input text-sm transition-all first:rounded-l-md first:border-l last:rounded-r-md"
+  />
+</template>
